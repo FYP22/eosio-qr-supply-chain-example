b3172f5fa3437b36af15dafe431acc1b79a3e9fc
Michael Harris <michael.harris@block.one>
2020-08-21 14:25:25 -0400

    Add comments in contract header and source files

--- contract/include/tracker.hpp
+++ contract/include/tracker.hpp
@@ -1,5 +1,6 @@
 #include <eosio/eosio.hpp>
 
+// location table
 struct [[eosio::table("location"), eosio::contract("tracker")]] location {
     uint64_t id;
     std::string location;
@@ -11,13 +12,16 @@ struct [[eosio::table("location"), eosio::contract("tracker")]] location {
 class tracker : eosio::contract {
     public:
 
-        tracker(eosio::name receiver, eosio::name code,  eosio::datastream<const char*> ds) : eosio::contract(receiver, code, ds) {}
+    // constructor
+    tracker(eosio::name receiver, eosio::name code,  eosio::datastream<const char*> ds) : eosio::contract(receiver, code, ds) {}
 
-        [[eosio::action]]
-        void create(uint64_t id, eosio::name account, std::string location);
+    // create an item to track in location table (adds new row)
+    [[eosio::action]]
+    void create(uint64_t id, eosio::name account, std::string location);
 
-        [[eosio::action]]
-        void update(uint64_t id, eosio::name account, std::string location);
+    // update an item that already exists in the location table
+    [[eosio::action]]
+    void update(uint64_t id, eosio::name account, std::string location);
 
-        location loc_table;
+    location loc_table;
 };
